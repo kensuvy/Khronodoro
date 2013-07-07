6a12212159c398bc988fde1948b3bab6a192d5e9
kensuvy <kensuvy@gmail.com>
2013-07-08 03:58:53 +0530

    added some files

---
+++ app/login.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<title>Khronodoro::Login</title>
+		<link rel="stylesheet" type="text/css" href="../css/main.css">
+	</head>
+	<body id="center">
+		<div id="popup">
+			<h1 align=center>Login</h1>
+			<form method="post" class="login" action="#">
+				<fieldset class="textbox">
+				
+				<label class="username">
+				<span>Username</span>
+				<input id="username" name="username" value="" type="text" autocomplete="on" placeholder="Username">
+				</label>
+				<p/>
+				<label class="password">
+				<span>Password</span>
+				<input id="password" name="password" value="" type="password" placeholder="Password">
+				</label>
+				<p/>	
+				<button class="submit button" type="button">Sign in</button>
+				<p>
+				<a class="forgot" href="reset.php">Forgot your password?</a>
+				</p>
+				</fieldset>
+			</form>
+		</div>
+	</body>
+</html>
+			
+							
---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>Khronodoro</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+	<body>
+		<header id="banner">
+			<h1><a href="#" id="webname">Khronodoro</a></h1>
+		</header>
+		<p/>
+		<div id="navbar">
+			<nav>
+				<ul>
+					<li><a href="#">Register</a></li>
+					<li><a href="#">Login</a></li>
+				</ul>
+			</nav>
+		</div>
+		<p/>
+		<div id="desc">
+			Khronodoro is a Getting-Things-Done app with a Pomodoro timer.
+		</div>
+	</body>
+</html>
